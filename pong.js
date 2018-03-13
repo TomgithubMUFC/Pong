ab6a8660c245347e820d9bf4140df677d8ededef
Harry Tomlinson <harry.t.123@hotmail.com>
2018-03-13 12:41:25 +0000

    LineDash

--- pong.js
+++ pong.js
@@ -4,11 +4,13 @@ var ctx = canvas.getContext("2d");
 var x = canvas.width / 2;
 var y = canvas.height - 350;
 
-function drawCenter() {
-    
-    ctx.beginPath();
-    ctx.rect(x, y, 50, 50);
-    ctx.fillStyle = "white";
-    ctx.fill();
-    ctx.closePath();
+function lineDash() {
+
+    ctx.setLineDash([10,5]);
+    ctx.moveTo(x, 0);
+    ctx.lineTo(x, canvas.height);
+    ctx.strokeStyle="white";
+    ctx.stroke();
 }
+
+lineDash();
