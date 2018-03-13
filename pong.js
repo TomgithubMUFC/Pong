d4ebea9d14ba677eea4ce4bba31cc72301842f01
DanielEastburn <danieleastburn@talktalk.net>
2018-03-13 14:53:16 +0000

    Start Screen

--- pong.js
+++ pong.js
@@ -60,21 +60,32 @@ function drawScore(){
 
 function drawStartMenu(){
     
-    ctx.font = "70px Comic Sans MS";
+    ctx.font = "200px Black Ops One";
     ctx.fillStyle = "white";
     ctx.textAlign = "center";
-    ctx.fillText("Pong", canvas.width/2, canvas.height/4); 
+    ctx.fillText("PONG", canvas.width/2, canvas.height/4); 
     
-    
-    ctx.font = "30px Comic Sans MS";
-    ctx.fillStyle = "white";
-    ctx.textAlign = "center";
-    ctx.fillText("Start", canvas.width/2, canvas.height/2);
-    
-    ctx.font = "30px Comic Sans MS";
+
+    ctx.font = "40px Black Ops One";
     ctx.fillStyle = "white";
     ctx.textAlign = "center";
     ctx.fillText("1 Player", canvas.width/2, canvas.height/2);
+    
+    ctx.font = "40px Black Ops One";
+    ctx.fillStyle = "white";
+    ctx.textAlign = "center";
+    ctx.fillText("2 Players Local", canvas.width/2, 420);
+
+    ctx.font = "40px Black Ops One";
+    ctx.fillStyle = "white";
+    ctx.textAlign = "center";
+    ctx.fillText("2 Players Online", canvas.width/2, 500);
+
+    ctx.beginPath();
+    ctx.lineWidth = "3";
+    ctx.strokeStyle = "lightgreen"
+    ctx.rect(400, 300, 400, 240);
+    ctx.stroke();
 
 
 }
@@ -92,5 +103,5 @@ function drawPong(){
 }
 
 
-setInterval(drawPong, 10);
+//setInterval(drawPong, 10);
 setInterval(drawPongStartScreen, 10);
